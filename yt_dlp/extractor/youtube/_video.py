7d482911361d80efe6b2ecd5f31292dc522fb2c7
Rei3100 <137330850+Rei3100@users.noreply.github.com>
2025-08-20 11:14:58 +0900

    Update _video.py

--- yt_dlp/extractor/youtube/_video.py
+++ yt_dlp/extractor/youtube/_video.py
@@ -2020,7 +2020,7 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
         if not player_url:
             return
 
-        requested_js_variant = self._configuration_arg('player_js_variant', [''])[0] or 'actual'
+        requested_js_variant = self._configuration_arg('player_js_variant', [''])[0] or 'main'
         if requested_js_variant in self._PLAYER_JS_VARIANT_MAP:
             player_id = self._extract_player_info(player_url)
             original_url = player_url
@@ -3831,10 +3831,6 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
         webpage, webpage_ytcfg, initial_data, is_premium_subscriber, player_responses, player_url = self._initial_extract(
             url, smuggled_data, webpage_url, webpage_client, video_id)
 
-        # langが反映されないリクエストを先に投げているので、順序を逆にして日本語の情報を取得している
-        # todo: 最初の方のリクエストを修正する。
-        player_responses.reverse()
-
         playability_statuses = traverse_obj(
             player_responses, (..., 'playabilityStatus'), expected_type=dict)
 
@@ -3854,9 +3850,33 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
             player_responses, (..., 'microformat', 'playerMicroformatRenderer'),
             expected_type=dict)
 
-        video_title = (video_details[0]['title']  # primary
-                       or get_first(video_details, 'title')
+        # Fallbacks in case player responses are missing metadata
+        initial_sdcr = traverse_obj(initial_data, (
+            'engagementPanels', ..., 'engagementPanelSectionListRenderer',
+            'content', 'structuredDescriptionContentRenderer', {dict}, any))
+        initial_description = traverse_obj(initial_sdcr, (
+            'items', ..., 'expandableVideoDescriptionBodyRenderer',
+            'attributedDescriptionBodyText', 'content', {str}, any))
+        # videoDescriptionHeaderRenderer also has publishDate/channel/handle/ucid, but not needed
+        initial_vdhr = traverse_obj(initial_sdcr, (
+            'items', ..., 'videoDescriptionHeaderRenderer', {dict}, any)) or {}
+        initial_video_details_renderer = traverse_obj(initial_data, (
+            'playerOverlays', 'playerOverlayRenderer', 'videoDetails',
+            'playerOverlayVideoDetailsRenderer', {dict})) or {}
+        initial_title = (
+            self._get_text(initial_vdhr, 'title')
+            or self._get_text(initial_video_details_renderer, 'title'))
+
+        translated_title = self._get_text(microformats, (..., 'title'))
+        video_title = ((self._preferred_lang and translated_title)
+                       or get_first(video_details, 'title')  # primary
+                       or translated_title
                        or search_meta(['og:title', 'twitter:title', 'title']))
+        if not video_title and initial_title:
+            self.report_warning(
+                'No title found in player responses; falling back to title from initial data. '
+                'Other metadata may also be missing')
+            video_title = initial_title
         translated_description = self._get_text(microformats, (..., 'description'))
         original_description = get_first(video_details, 'shortDescription')
         video_description = (
@@ -3864,6 +3884,8 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
             # If original description is blank, it will be an empty string.
             # Do not prefer translated description in this case.
             or original_description if original_description is not None else translated_description)
+        if video_description is None:
+            video_description = initial_description
 
         multifeed_metadata_list = get_first(
             player_responses,
@@ -4022,6 +4044,12 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
         if needs_live_processing:
             self._prepare_live_from_start_formats(
                 formats, video_id, live_start_time, url, webpage_url, smuggled_data, live_status == 'is_live')
+        elif live_status != 'is_live':
+            # Handle preroll waiting period
+            preroll_sleep = int_or_none(self._configuration_arg('preroll_sleep', [None])[0], default=6)
+            available_at = int(time.time()) + preroll_sleep
+            for fmt in formats:
+                fmt['available_at'] = available_at
 
         formats.extend(self._extract_storyboard(player_responses, duration))
 
